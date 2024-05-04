1de16ad62cba9d9ecad202e3ca33878595c21f66
Aju Alex <ajualex66@gmail.com>
2024-05-04 15:08:14 +0530

    some useful packages and a font asset added

---
+++ android/app/src/main/kotlin/com/example/tripled/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tripled
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/tripledhome.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TripleDHome extends StatelessWidget {
+  const TripleDHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TripleD"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(18),
+        shrinkWrap: true,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
