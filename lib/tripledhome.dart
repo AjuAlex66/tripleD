5269079f8788684b4548aaac80bae19907c95dbd
Aju Alex <ajualex66@gmail.com>
2024-05-04 15:19:48 +0530

    task 2 completed - state field value changes when country changes and vice versa

--- lib/tripledhome.dart
+++ lib/tripledhome.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tripled/common/roundedcontainer.dart';
 import 'package:tripled/utils/changerclass.dart';
+import 'package:tripled/utils/helper.dart';
 import 'package:tripled/utils/initializer.dart';
 
 class TripleDHome extends StatelessWidget {
@@ -17,7 +18,7 @@ class TripleDHome extends StatelessWidget {
         padding: const EdgeInsets.all(18),
         shrinkWrap: true,
         children: [
-           // task 1
+          // task 1
           Consumer<Changer>(
             builder: (context, value, child) => RoundedContainer(
               title: "#1 Drop down - no plugin",
@@ -38,6 +39,68 @@ class TripleDHome extends StatelessWidget {
                       .toList(),
                   onChanged: (value) {
                     Initializer.tripleDropValue = value;
+                    context.read<Changer>().justChange();
+                  },
+                ),
+              ],
+            ),
+          ),
+          //-------------------------------------------------------
+          //task 2
+          Consumer<Changer>(
+            builder: (context, value, child) => RoundedContainer(
+              title: "#2 Country and State",
+              children: [
+                const Text("Country"),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: Initializer.tripleCountry,
+                  items: Initializer.tripleCountryList
+                      .map((e) => DropdownMenuItem<String>(
+                          value: e['country'],
+                          child: Text(
+                            e['country'],
+                            style: const TextStyle(),
+                          )))
+                      .toList(),
+                  onChanged: (value) {
+                    Initializer.tripleCountry = value;
+                    for (var e in Initializer.tripleCountryList) {
+                      if (e['country'] == value) {
+                        e['selected'] = true;
+                        break;
+                      } else {
+                        e['selected'] = false;
+                      }
+                    }
+                    context.read<Changer>().justChange();
+                  },
+                ),
+                Helper.allowHeight(15),
+                const Text("State"),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: Initializer.tripleCountryList
+                      .where((element) => element['selected'])
+                      .first['states'],
+                  items: Initializer.tripleCountryList
+                      .map((e) => DropdownMenuItem<String>(
+                          value: e['states'],
+                          child: Text(
+                            e['states'],
+                            style: const TextStyle(),
+                          )))
+                      .toList(),
+                  onChanged: (value) {
+                    for (var e in Initializer.tripleCountryList) {
+                      if (e['states'] == value) {
+                        Initializer.tripleCountry = e['country'];
+                        e['selected'] = true;
+                        break;
+                      } else {
+                        e['selected'] = false;
+                      }
+                    }
                     context.read<Changer>().justChange();
                   },
                 ),
--- lib/utils/initializer.dart
+++ lib/utils/initializer.dart
@@ -1,4 +1,9 @@
 class Initializer {
-  static String? tripleDropValue = 'View';
+  static String? tripleDropValue = 'View', tripleCountry = 'IN';
   static List<String> tripleDropList = ['View', 'Edit', 'Send', 'Delete'];
+  static List<Map<String, dynamic>> tripleCountryList = [
+    {"country": "IN", "states": "KA, KL, TN and MH", "selected": true},
+    {"country": "US", "states": "AL, DE and GA", "selected": false},
+    {"country": "CA", "states": "ON, QC and BC", "selected": false},
+  ];
 }
